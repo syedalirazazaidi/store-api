67de1ef3d5c9867c606bb43c245a6033e883e174
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-10 10:54:06 +0500

    add product and populate data base

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,11 @@ const connectDB = require('./db/connect')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
+const productsRouter = require('./routes/product')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
