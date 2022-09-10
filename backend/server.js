583120b36f9d41ea0a1064890855e79f035dfc82
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-10 09:44:44 +0500

    connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,26 @@
-require('dotenv').config();
-require('express-async-errors');
+const express = require('express')
+const colors = require('colors')
+require('dotenv').config()
+require('express-async-errors')
 
-const express = require('express');
-const app = express();
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+const port = process.env.PORT || 3000
+
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
